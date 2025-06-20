c13d53bd48f8759e50a59b7635f99cfa24df8799
Hiyashree <hiyasarma90@gmail.com>
2025-06-20 23:41:41 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -10,7 +10,7 @@
   }
 
   try {
-    const response = await fetch("http://localhost:3000/analyze", {
+    const response = await fetch("https://knowemotion.onrender.com/analyze", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
